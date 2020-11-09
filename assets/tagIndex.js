f7aa52d1d4bd5d4aeba7052880c15cf7bf534282
C-D-Lewis <Bonsitm@gmail.com>
2020-11-09 21:23:09 +0000

    Fix typo, update assets

--- assets/tagIndex.js
+++ assets/tagIndex.js
@@ -227,7 +227,6 @@ window.tagIndex = {
     "2018-03-30-FitBit-Development-Round-1-Review-and-Future-Plans.json",
     "2018-05-19-FitBit-Development-Round-2.json",
     "2020-10-04-Deploying-With-Terraform.json",
-    "2020-11-09-Mimicker-A-Simple-Twitter-Bot.json",
     "2020-11-09-Mimicker-A-Simple-Twitter-Personality.json"
   ],
   "Raspberry Pi": [
@@ -279,7 +278,6 @@ window.tagIndex = {
     "2020-08-15-Block-World-Lives-On.json",
     "2020-08-22-Creating-a-Browser-based-Party-Game.json",
     "2020-10-04-Deploying-With-Terraform.json",
-    "2020-11-09-Mimicker-A-Simple-Twitter-Bot.json",
     "2020-11-09-Mimicker-A-Simple-Twitter-Personality.json"
   ],
   "FitBit": [
