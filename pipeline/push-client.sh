1dec396d05cd19cd8e7bb3377a4add783579ca9f
C-D-Lewis <Bonsitm@gmail.com>
2021-09-25 10:01:58 +0100

    0.13 upgrade

--- pipeline/push-client.sh
+++ pipeline/push-client.sh
@@ -12,7 +12,7 @@ sed -i.bak "s/DATENOW/$DATENOW/g" index.html
 aws s3 cp index.html $BUCKET
 aws s3 cp favicon.ico $BUCKET
 aws s3 sync src $BUCKET/src
-aws s3 sync assets $BUCKET/assets
+aws s3 sync assets $BUCKET/assets --exclude postHistory.js
 aws s3 sync styles $BUCKET/styles
 aws s3 sync feed $BUCKET/feed
 
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,11 @@
 module "main" {
-  source           = "./infrastructure"
-  region           = var.region
-  project_name     = var.project_name
-  vpc_id           = var.vpc_id
-  zone_id          = var.zone_id
-  domain_name      = var.domain_name
-  certificate_arn  = var.certificate_arn
+  source          = "./infrastructure"
+  region          = var.region
+  project_name    = var.project_name
+  vpc_id          = var.vpc_id
+  zone_id         = var.zone_id
+  domain_name     = var.domain_name
+  certificate_arn = var.certificate_arn
 }
 
 provider "aws" {
@@ -14,12 +14,15 @@ provider "aws" {
 
 terraform {
   required_providers {
-    aws = "~> 2.70"
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.60"
+    }
   }
 
   backend "s3" {
-    bucket  = "chrislewis.me.uk-tfstate"
-    key     = "blog"
-    region  = "us-east-1"
+    bucket = "chrislewis.me.uk-tfstate"
+    key    = "blog"
+    region = "us-east-1"
   }
 }
