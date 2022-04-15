70138e0f69e76920079c293d131df8c90764d8e0
C-D-Lewis <Bonsitm@gmail.com>
2022-04-15 16:01:02 +0100

    Update pipeline

--- pipeline/invalidation.sh
+++ pipeline/invalidation.sh
@@ -4,10 +4,13 @@ set -eu
 
 SITE_URL=$1
 
+# Get CloudFront distribution ID
 CF_DIST_ID=$(aws cloudfront list-distributions | jq -r ".DistributionList.Items[] | select(.Aliases.Items[0] == \"$SITE_URL\") | .Id")
 
+# Create new invalidation
 RES=$(aws cloudfront create-invalidation --distribution-id $CF_DIST_ID --paths "/*")
 INVALIDATION_ID=$(echo $RES | jq -r '.Invalidation.Id')
 
+# Wait for invalidation complete
 echo "Waiting for invalidation-completed for $INVALIDATION_ID..."
 aws cloudfront wait invalidation-completed --distribution-id $CF_DIST_ID --id $INVALIDATION_ID
