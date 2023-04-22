43cf7604eb6e69a849038a49d09ccdb7faa01941
C-D-Lewis <Bonsitm@gmail.com>
2023-04-22 13:49:05 +0100

    Remove alt domain name

--- terraform/main.tf
+++ terraform/main.tf
@@ -26,6 +26,5 @@ module "main" {
   project_name    = "blog"
   zone_id         = "Z05682866H59A0KFT8S"
   domain_name     = "blog.chrislewis.me.uk"
-  alt_domain_name = "www.blog.chrislewis.me.uk"
   certificate_arn = "arn:aws:acm:us-east-1:617929423658:certificate/a69e6906-579e-431d-9e4c-707877d325b7"
 }
