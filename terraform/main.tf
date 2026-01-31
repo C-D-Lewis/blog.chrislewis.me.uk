a0e592ed1acfdbf1c1f823f9b20006c841a0decc
C-D-Lewis <Bonsitm@gmail.com>
2026-01-31 21:36:05 +0000

    update

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,12 +3,12 @@ provider "aws" {
 }
 
 terraform {
-  required_version = "= 1.2.9"
+  required_version = "~> 1.5.0"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "= 4.31.0"
+      version = "~> 4.31.0"
     }
   }
 
